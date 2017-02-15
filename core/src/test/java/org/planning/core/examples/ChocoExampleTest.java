34b4dd4ec7b226df5a91e255f575d77a8c095b18
Pascal Stammer <stammer@deichbrise.de>
2017-02-15 10:21:10 +0100

    cleanup, instructor time constraints

--- core/src/test/java/org/planning/core/examples/ChocoExampleTest.java
+++ core/src/test/java/org/planning/core/examples/ChocoExampleTest.java
@@ -69,13 +69,26 @@ public class ChocoExampleTest extends AbstractCoreTest {
             model.ifThen( vars[i][1].eq( 4 ).decompose(), vars[i][3].eq( 2 ).decompose());
 
             // Instructor - Time Constraints
+            model.ifThen( vars[i][2].eq( 0 ).decompose(), model.member( vars[i][0], new int[]{1, 2, 3} ) );
+            model.ifThen( vars[i][2].eq( 1 ).decompose(), model.member( vars[i][0], new int[]{0, 1, 2} ) );
+            model.ifThen( vars[i][2].eq( 2 ).decompose(), model.member( vars[i][0], new int[]{3, 4} ) );
+            model.ifThen( vars[i][2].eq( 3 ).decompose(), model.member( vars[i][3], new int[]{1, 2} ));
+            model.ifThen( vars[i][2].eq( 4 ).decompose(), model.member( vars[i][3], new int[]{0, 1} ));
+
         }
 
 
         Solution solution = model.getSolver().findSolution();
 
+        final String[] map = new String[]{"Day: ", "Room: ", "Instructor: ", "Slot: "};
         if ( solution != null ) {
-            System.out.println( solution.toString() );
+            for(int i = 0; i < numberOfLectures; i++) {
+                System.out.print( "Lecture "+ (i + 1) + ": ( " );
+                for(int j = 0; j < 4; j++) {
+                    System.out.print( map[j] + Integer.toString( vars[i][j].getValue() ) + " ");
+                }
+                System.out.println( " )" );
+            }
         }
     }
 }
